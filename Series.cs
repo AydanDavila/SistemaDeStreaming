eef263e3cfa87a396475f6d09d6f3236d7c503e9
Aydan <aydan.navarro328@std.uni.edu.ni>
2026-04-16 20:53:08 -0600

    Agregar archivos de proyecto.

---
+++ Series.cs
@@ -0,0 +1,54 @@
+﻿public class Series : Contenido
+{
+    private int _numTemporadas;
+    private int _numEpisodios;
+
+    public Series(string? titulo, string? genero, int duracionMinutos, string? descripcion, int id, int anioLanzamiento, string categoria, int numTemporadas, int numEpisodios) : base(titulo, genero, duracionMinutos, descripcion, id, anioLanzamiento, categoria)
+    {
+        NumTemporadas = numTemporadas;
+        NumEpisodios = numEpisodios;
+    }
+
+
+    public int NumTemporadas
+    {
+        get => _numTemporadas;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Número de temporadas inválido");
+            }
+            _numTemporadas = value;
+        }
+    }
+    public int NumEpisodios
+    {
+        get => _numEpisodios;
+        set
+        {
+            if (value < 1)
+            {
+
+                throw new ArgumentOutOfRangeException("Cantidad de episodios inválida");
+            }
+            _numEpisodios = value;
+        }
+    }
+
+    /// <summary>
+    /// Sobrescribe la representación visual para incluir los controles específicos de 
+    /// reproducción y las opciones de navegación propias de este tipo de contenido.
+    /// </summary>
+    public override void Dibujar()
+    {
+        base.Dibujar();
+        Console.WriteLine("\n [1] ▌▌ Pausar    [2] i Información      [3] «« Retroceder    ");
+        Console.WriteLine(" [4] »» Adelantar      [5] > Siguiente Capítulo     [6] < Capítulo anterior       [0] ⏹️ Salir");
+        Console.Write("\nElige una opción: ");
+        Console.WriteLine("==================================================");
+
+    }
+    
+}
+
---
+++ Usuario.cs
@@ -0,0 +1,94 @@
+﻿public class Usuario
+{
+    private string? _nombre;
+    private string? _email;
+    private string? _contraseña;
+    private int _pin;
+    private int _edad;
+
+    public Usuario(string? nombre, string? email, string? contraseña, int pin, int edad)
+    {
+        Nombre = nombre;
+        Email = email;
+        Contraseña = contraseña;
+        Pin = pin;
+        Edad = edad;
+    }
+
+    
+
+public int Edad
+{
+    get => _edad;
+    set
+    {
+        if (value < 18 || value > 120) 
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "Edad no válida.");
+        }
+        _edad = value;
+    }
+}
+
+    public string? Nombre
+    {
+        get { return _nombre; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("El nombre no puede estar vacío.");
+            }
+            _nombre = value;
+        }
+    }
+
+
+    public string? Email
+    {
+        get { return _email; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("El correo electrónico no puede estar vacío.");
+            }
+            _email = value;
+        }
+    }
+
+
+    public string? Contraseña
+    {
+
+        get { return _contraseña; }
+        set
+        {
+
+            if (string.IsNullOrWhiteSpace(value)||value.Contains(" "))
+            {
+                throw new ArgumentException("La contraseña no puede estar vacía o contener espacios.");
+            }
+            _contraseña = value;
+
+        }
+    }
+
+
+    public int Pin
+    {
+        get
+        {
+            return _pin;
+        }
+        set
+        {
+            if (value.ToString().Count() != 4)
+            {
+                throw new ArgumentOutOfRangeException ("El PIN debe ser de 4 digitos.");
+            }
+            _pin = value;
+        }
+    }
+}
+
